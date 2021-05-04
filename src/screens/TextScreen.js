0f42adf6d846f7edb26d15b1ff460893ccf480a3
Linda Levin <linda@onlinecoach.nu>
2021-05-04 21:23:16 +0200

    init

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+
+const TextScreen = () => {
+return <View>
+    <Text>Text Screen</Text>
+</View>
+};
+
+const styles = StyleSheet.create({});
+
+export default TextScreen;
